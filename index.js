67fbf61df9dbcc18df0cd6dd9837fb1349534693
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-14 13:00:14 +0530

    Server File

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require('express')
+const app = new express()
+const links = require('./links.json')
+const fetch = require('node-fetch')
 
+app.get('/', (req, res) => console.log('Ping Received'))
+
+setInterval(() => {
+  console.log('\nNew Wave at ' + Date.now())
+  links.forEach(l => {
+    fetch(l)
+    console.log('Pinged ' + l)
+  }
+  console.log(' ')
+}, 1000)
+
+app.listen(2020)
